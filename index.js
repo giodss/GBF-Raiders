8a53669674934b3e51849653befcd5c839d3811c
giodss <giodss@users.noreply.github.com>
2017-10-11 00:46:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -166,6 +166,7 @@ function StartTwitterStream() {
 		stream.on( 'data', function ( event ) {
 
 			TimedLogger( "Tweet found." );
+			console.log(event.text);
 			let room = searchTextForRaids( event.text );
 			var message = "No Twitter Message.";
 			var language = "JP";
